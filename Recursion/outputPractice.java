634fc9552fcfaf3a1232a2a84bc1b7878a89a040
Saurabh Kumar <83708370+SaurabhK05@users.noreply.github.com>
2021-06-01 17:46:37 +0530

    New Commit

---
+++ Recursion/outputPractice.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class outputPractice {
+	static void fun(int n) {
+		if( n == 0)
+			return;
+		fun(n/2);
+		System.out.print(n%2+" ");
+	}
+
+	public static void main(String[] args) {
+		fun(6);
+
+	}
+
+}
